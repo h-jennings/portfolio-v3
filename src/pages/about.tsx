a1eed7f8748c599d9e53211bb749a2a9a265d391
Hunter Jennings <jenningsdhunter@gmail.com>
2021-12-19 12:45:52 -0500

    WIP: working on about page markup and styling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,8 @@
-import { Stack } from '@/components/Stack';
+import { Box } from '@components/Box';
 import { SplitGrid } from '@components/layout/SplitGrid';
+import { Stack } from '@components/Stack';
 import { BodyText, PageHeader } from '@components/Text';
+import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
 import { PATHS } from '@utils/constants/paths.constants';
 import { NextPage } from 'next';
 import { NextSeo, NextSeoProps } from 'next-seo';
@@ -23,14 +25,14 @@ const About: NextPage = () => {
       <SplitGrid>
         <Stack gap='4'>
           <PageHeader>About</PageHeader>
-          <Stack gap='3'>
+          <Stack style={{ maxWidth: 390 }} gap='2'>
             <BodyText>
-              Hey there, I&apos;m Hunter. I&apos;ve been wrangling pixels and
-              shapes since &apos;17. I create beautiful user interfaces with
-              cutting-edge web technologies. My primary areas of focus are
-              component architecture, design systems, and application state
-              management. Over the years, I&apos;ve worked cross-functionally
-              with high caliber design teams on complex problems.
+              I&apos;ve been wrangling pixels and shapes since &apos;17. I
+              create beautiful user interfaces with cutting-edge web
+              technologies. My primary areas of focus are component
+              architecture, design systems, and application state management.
+              Over the years, I&apos;ve worked cross-functionally with high
+              caliber design teams on complex problems.
             </BodyText>
             <BodyText>
               I&apos;m a life long learner who can&apos;t wait to start the next
@@ -38,7 +40,18 @@ const About: NextPage = () => {
             </BodyText>
           </Stack>
         </Stack>
-        <div>content right</div>
+        <Box>
+          <Box style={{ maxWidth: 511 }}>
+            <AspectRatioPrimitive.Root ratio={1}>
+              <Box
+                css={{
+                  height: '$full',
+                  backgroundColor: '$slate8',
+                }}
+              ></Box>
+            </AspectRatioPrimitive.Root>
+          </Box>
+        </Box>
       </SplitGrid>
     </>
   );
