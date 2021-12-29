885d8d9cc4245a89d1aa69b8fbefd3b53b32c254
Hunter Jennings <jenningsdhunter@gmail.com>
2021-12-28 20:57:03 -0500

    WIP: working on the new homepage design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import { Stack } from '@/components/Stack';
 import { styled } from '@/stitches.config';
-import { H1, Text } from '@components/Text';
+import { H1, H2, Paragraph } from '@components/Text';
 import type { NextPage } from 'next';
 import { NextSeo, NextSeoProps } from 'next-seo';
 
@@ -21,9 +22,34 @@ const Index: NextPage = () => {
   return (
     <>
       <NextSeo {...SEO} />
-      <Text family='serif' size='2'>
-        This is a test
-      </Text>
+      <Stack gap='3xl'>
+        {/* Intro */}
+        <Stack as='section' gap='xl'>
+          <Stack gap='m'>
+            <H1 size='2' family='serif' leading='tight'>
+              Hunter Jennings
+            </H1>
+            <Paragraph style={{ maxWidth: 600 }}>
+              Frontend ui engineer interested in design systems, component
+              architectures, and React.
+            </Paragraph>
+          </Stack>
+          <Stack gap='xs'>
+            <H2 color='2' aria-label="What I'm up to now" size='1'>
+              Now
+            </H2>
+            <Paragraph>
+              Currently working as a Frontend Developer for an award-winning
+              digital creative agency Elegant Seagulls.
+            </Paragraph>
+          </Stack>
+        </Stack>
+
+        {/* Work */}
+        <Stack as='section' gap='s'>
+          <H1>Selected work</H1>
+        </Stack>
+      </Stack>
     </>
   );
 };
