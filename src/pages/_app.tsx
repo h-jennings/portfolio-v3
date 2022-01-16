3a5803363ef5d997210cc24173afd9f12de149b7
Hunter Jennings <jenningsdhunter@gmail.com>
2022-01-16 11:27:21 -0500

    Add: adding plausible script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,29 @@ import { RootLayout } from '@common/components/layout/RootLayout';
 import { useVisualViewportHeight } from '@common/hooks/use-visual-viewport-height';
 import { ThemeProvider } from 'next-themes';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   useVisualViewportHeight();
 
   return (
-    <ThemeProvider
-      disableTransitionOnChange
-      attribute='class'
-      value={{ dark: 'dark-theme', light: lightTheme.className }}
-      defaultTheme='system'
-    >
-      <RootLayout>
-        <Component {...pageProps} />
-      </RootLayout>
-    </ThemeProvider>
+    <>
+      <Script
+        defer
+        data-domain='hunterjennings.dev'
+        src='https://plausible.io/js/plausible.js'
+      />
+      <ThemeProvider
+        disableTransitionOnChange
+        attribute='class'
+        value={{ dark: 'dark-theme', light: lightTheme.className }}
+        defaultTheme='system'
+      >
+        <RootLayout>
+          <Component {...pageProps} />
+        </RootLayout>
+      </ThemeProvider>
+    </>
   );
 };
 export default MyApp;
