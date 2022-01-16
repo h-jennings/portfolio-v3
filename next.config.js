6d140c8a30ce13a241d2eac25fbe76dd13519c1d
Hunter Jennings <jenningsdhunter@gmail.com>
2022-01-16 00:10:51 -0500

    Add: adding redirects

--- next.config.js
+++ next.config.js
@@ -15,6 +15,30 @@ const nextConfig = {
   images: {
     domains: ['openweathermap.org'],
   },
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/dod',
+        destination: '/work/dwr',
+        permanent: true,
+      },
+      {
+        source: '/portfolio-v1',
+        destination: 'https://v2.hunterjennings.dev/portfolio-v1',
+        permanent: true,
+      },
+      {
+        source: '/caffeinator',
+        destination: 'https://v2.hunterjennings.dev/caffeinator',
+        permanent: true,
+      },
+    ];
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/,
