7552124eafd5be17668362cc50fd464a84c91028
Hunter Jennings <jenningsdhunter@gmail.com>
2022-01-15 23:42:41 -0500

    Add: #34 adding code block remark plugin

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { lightTheme } from '@/stitches.config';
-import '@/styles/global.css';
+import '@/styles/global.scss';
 import { RootLayout } from '@common/components/layout/RootLayout';
 import { useVisualViewportHeight } from '@common/hooks/use-visual-viewport-height';
 import { ThemeProvider } from 'next-themes';
