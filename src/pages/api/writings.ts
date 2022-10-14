940045114a232ccecc98e3a427012d08b1420481
Hunter Jennings <jenningsdhunter@gmail.com>
2022-10-14 15:59:12 -0400

    changing fs

--- src/pages/api/writings.ts
+++ src/pages/api/writings.ts
@@ -1,42 +1,40 @@
 import { PATHS } from '@utils/common/constants/paths.constants';
 import { getMdxMetadata } from '@utils/common/helpers/mdx-data.helpers';
 import { FrontMatterPreParse } from '@utils/common/types/mdx-data';
-import fs from 'fs/promises';
+import { readdirSync, readFileSync } from 'fs';
 import matter from 'gray-matter';
 import { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
 
-const getFileSource = (p1: string, p2: string): Promise<Buffer> => {
+const getFileSource = (p1: string, p2: string): Buffer => {
   const filePath = path.join(p1, p2);
-  return fs.readFile(filePath);
+  return readFileSync(filePath);
 };
 
-export const getWritings = async () => {
+export const getWritings = () => {
   const dir = path.resolve(process.cwd(), 'src');
   const WRITINGS_PATH = path.join(dir, PATHS.writings);
 
-  const writingsFilePaths = await fs.readdir(WRITINGS_PATH).then((path) => {
-    // Only include md(x) files
-    return path.filter((path) => /\.mdx?$/.test(path));
+  const writingsFilePaths = readdirSync(WRITINGS_PATH).filter((path) =>
+    /\.mdx?$/.test(path),
+  );
+
+  const writings = writingsFilePaths.map((fileName) => {
+    const fileSource = getFileSource(WRITINGS_PATH, fileName);
+    const { content, data } = matter(fileSource);
+
+    return {
+      content,
+      fileName,
+      metaData: getMdxMetadata(content, data as FrontMatterPreParse),
+    };
   });
 
-  const writings = await Promise.all(
-    writingsFilePaths.map(async (fileName) => {
-      const fileSource = await getFileSource(WRITINGS_PATH, fileName);
-      const { content, data } = matter(fileSource);
-
-      return {
-        content,
-        fileName,
-        metaData: getMdxMetadata(content, data as FrontMatterPreParse),
-      };
-    }),
-  );
   return writings;
 };
 
-const writings = async (_req: NextApiRequest, res: NextApiResponse) => {
-  const writings = await getWritings();
+const writings = (_req: NextApiRequest, res: NextApiResponse) => {
+  const writings = getWritings();
 
   res.status(200).json(writings);
 };
