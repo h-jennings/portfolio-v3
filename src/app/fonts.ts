292c1bd8eb67f10e1521f5818cc0e02866a15e17
Hunter Jennings <hunterjennings@hunters-mbp.lan>
2023-06-17 12:37:52 -0400

    feat(appDir): fonts and root layout

---
+++ src/app/fonts.ts
@@ -0,0 +1,50 @@
+import localFont from 'next/font/local';
+
+export const basierCircle = localFont({
+  display: 'swap',
+  preload: true,
+  fallback: ['system-ui', 'sans-serif'],
+  variable: '--font-sans',
+  src: [
+    {
+      path: '../../public/fonts/basiercircle-regular-webfont.woff2',
+      weight: '400',
+    },
+    {
+      path: '../../public/fonts/basiercircle-bold-webfont.woff2',
+      weight: '700',
+    },
+  ],
+});
+
+export const untitledSerif = localFont({
+  display: 'swap',
+  preload: false,
+  fallback: ['serif'],
+  variable: '--font-serif',
+  src: [
+    {
+      path: '../../public/fonts/untitled-serif-regular-italic.woff2',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../../public/fonts/untitled-serif-bold-italic.woff2',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+});
+
+export const jetbrainsMono = localFont({
+  display: 'swap',
+  preload: false,
+  fallback: ['monospace'],
+  variable: '--font-mono',
+  src: [
+    {
+      path: '../../public/fonts/jetbrainsmono-regular.woff2',
+      weight: '400',
+    },
+  ],
+});
