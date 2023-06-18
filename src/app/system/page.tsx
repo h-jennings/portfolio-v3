8dcd27b18cd71370b9326128f558419f2ad21def
Hunter Jennings <hunterjennings@hunters-mbp.lan>
2023-06-18 07:45:34 -0400

    feat(panda): adding panda lib

--- src/app/system/page.tsx
+++ src/app/system/page.tsx
@@ -1,3 +1,19 @@
+import { css } from 'ds/css';
+import { vstack, container } from 'ds/patterns';
+
 export default function SystemPage() {
-  return <div style={{ fontFamily: 'var(--font-sans)' }}>System Page</div>;
+  return (
+    <div className={container()}>
+      <div
+        className={vstack({
+          gap: '10',
+        })}
+      >
+        <h1 className={css({ fontSize: '4xl', color: 'gray.500' })}>
+          System Page
+        </h1>
+        <div>System Page</div>
+      </div>
+    </div>
+  );
 }
