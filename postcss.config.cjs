a67b56d8d30b55441c6a4c390e7c7ac501b21f58
Hunter Jennings <hunterjennings@hunters-mbp.lan>
2023-06-19 17:03:50 -0400

    add: postcss plugins and styling now text

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,18 @@
 module.exports = {
-  plugins: {
-    '@pandacss/dev/postcss': {},
-  },
-}
+  plugins: [
+    '@pandacss/dev/postcss',
+    'postcss-flexbugs-fixes',
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
+};
