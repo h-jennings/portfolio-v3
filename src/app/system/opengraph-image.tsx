ecec856aeef5af89c5d8314ca85154ec08ef73e9
Hunter Jennings <hunterjennings@hunters-mbp.lan>
2023-06-20 10:59:53 -0400

    remove system test page

--- src/app/system/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import { ogTemplate } from '../_utils/og-template';
-
-export const runtime = 'edge';
-export const alt =
-  'Hunter Jennings - Frontend ui engineer interested in design systems, component architectures, and React.';
-
-export default async function Image() {
-  return await ogTemplate({ title: 'System', sub: 'Opengraph Image Test' });
-}
