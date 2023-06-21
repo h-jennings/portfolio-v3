2ce03ebb90f5a1c44518ec55dee5abc94d57f1ae
Hunter Jennings <jenningsdhunter@gmail.com>
2023-06-21 19:40:52 -0400

    fix: file locations

---
+++ src/app/writing/opengraph-image.tsx
@@ -0,0 +1,12 @@
+import { ogTemplate } from '@/app/_utils/og-template';
+
+export const runtime = 'edge';
+export const alt =
+  'Writing - Thoughts on software, books, life, and any opinions I have at a moment in time.';
+
+export default async function Image() {
+  return await ogTemplate({
+    title: 'Writing',
+    sub: 'Thoughts on software, books, life, and any opinions I have at a moment in time.',
+  });
+}
