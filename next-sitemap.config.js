489db7701d4b3f28eaf6173b1285b24cd5f96b06
Hunter Jennings <jenningsdhunter@gmail.com>
2023-06-23 12:09:57 -0400

    fix: sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,15 @@
+/** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.hunterjennings.dev',
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap-index.xml', '/writing/element-test'],
-  additionalSitemaps: [
-    'https://www.hunterjennings.dev/server-sitemap-index.xml',
+  exclude: [
+    '/server-sitemap-index.xml',
+    '/work/server-sitemap.xml',
+    '/writing/element-test',
   ],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.hunterjennings.dev/server-sitemap-index.xml',
+    ],
+  },
 };
